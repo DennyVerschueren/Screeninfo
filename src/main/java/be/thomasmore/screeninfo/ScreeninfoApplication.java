e60d67a79347329247efc36e3c4220d59b036440
DennyVerschueren <r0952915@student.thomasmore.be>
2023-04-23 10:19:50 +0200

    Project start

---
+++ src/main/java/be/thomasmore/screeninfo/ScreeninfoApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.screeninfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeninfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreeninfoApplication.class, args);
+    }
+
+}
