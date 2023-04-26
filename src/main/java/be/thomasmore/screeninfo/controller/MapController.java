0e5b10c0a14aae0ff26b93fd42cba13ce06d45e1
Lande <r0781198@student.thomasmore.be>
2023-04-26 17:40:08 +0200

    gelukt om spots van de repositorie te laten verschijnen op de map

--- src/main/java/be/thomasmore/screeninfo/controller/MapController.java
+++ src/main/java/be/thomasmore/screeninfo/controller/MapController.java
@@ -1,15 +1,25 @@
 package be.thomasmore.screeninfo.controller;
 
+import be.thomasmore.screeninfo.model.Spot;
+import be.thomasmore.screeninfo.repositories.SpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class MapController {
 
+    @Autowired
+    private SpotRepository spotRepository;
     @GetMapping("/map")
     public String defaultMapView(Model model){
 
+        List<Spot> spots = spotRepository.findAll();
+
+        model.addAttribute("spots",spots);
         return "map";
     }
 
--- src/main/java/be/thomasmore/screeninfo/repositories/SpotRepository.java
+++ src/main/java/be/thomasmore/screeninfo/repositories/SpotRepository.java
@@ -3,6 +3,11 @@ package be.thomasmore.screeninfo.repositories;
 import be.thomasmore.screeninfo.model.Spot;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SpotRepository extends CrudRepository<Spot,Integer> {
+
+    @Override
+    public List<Spot> findAll();
 
 }
