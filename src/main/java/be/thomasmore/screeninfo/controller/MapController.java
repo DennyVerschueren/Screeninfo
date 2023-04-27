b145dcaf4022eca19194962c0fcc488e49c4e1d1
Lande <r0781198@student.thomasmore.be>
2023-04-27 15:50:16 +0200

    verschillende spot hebben zijn eigen kluer gebaseerd op het type van het spot

--- src/main/java/be/thomasmore/screeninfo/controller/MapController.java
+++ src/main/java/be/thomasmore/screeninfo/controller/MapController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -18,9 +19,25 @@ public class MapController {
     public String defaultMapView(Model model){
 
         List<Spot> spots = spotRepository.findAll();
+        List<Spot> toilletes = new ArrayList<Spot>();
+        List<Spot> festivals = new ArrayList<Spot>();
+        List<Spot> voedselKraampjes = new ArrayList<Spot>();
+        for (Spot spot: spots) {
+            System.out.println(spot.getSpotType());
+            if(spot.getSpotType().equals("TOILET")){
+                toilletes.add(spot);
+            }
+            else if(spot.getSpotType().equals("FESTIVAL")){
+                festivals.add(spot);
+            }
+            else if(spot.getSpotType().equals("VOEDSELKRAAM")){
+               voedselKraampjes.add(spot);
+            }
+        }
 
-        model.addAttribute("spots",spots);
+        model.addAttribute("toilletes",toilletes);
+        model.addAttribute("festivals",festivals);
+        model.addAttribute("voedselKraampjes",voedselKraampjes);
         return "map";
     }
-
 }
