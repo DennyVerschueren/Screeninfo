0b841e0a497b769a2cd6187c97f819f32382043e
salihtzc <salihtzc.1997@gmail.com>
2023-05-02 12:58:39 +0200

    addInterceptors

--- src/main/java/be/thomasmore/screeninfo/config/MessageConfig.java
+++ src/main/java/be/thomasmore/screeninfo/config/MessageConfig.java
@@ -4,6 +4,7 @@ package be.thomasmore.screeninfo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -26,6 +27,11 @@ public class MessageConfig implements WebMvcConfigurer {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
