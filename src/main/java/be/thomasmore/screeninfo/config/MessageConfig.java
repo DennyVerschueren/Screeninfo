79129a9293bf88f96f5959de42cc819d63c941b2
salihtzc <salihtzc.1997@gmail.com>
2023-05-02 12:55:06 +0200

    MessageConfig class gemaakt

---
+++ src/main/java/be/thomasmore/screeninfo/config/MessageConfig.java
@@ -0,0 +1,12 @@
+package be.thomasmore.screeninfo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MessageConfig implements WebMvcConfigurer {
+
+
+
+}
