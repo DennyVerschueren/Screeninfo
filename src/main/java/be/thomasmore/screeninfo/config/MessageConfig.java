1f962b5397e25a850444d1fc4c15e0e591ccf249
salihtzc <salihtzc.1997@gmail.com>
2023-05-02 12:56:23 +0200

    locale resolver

--- src/main/java/be/thomasmore/screeninfo/config/MessageConfig.java
+++ src/main/java/be/thomasmore/screeninfo/config/MessageConfig.java
@@ -1,12 +1,23 @@
 package be.thomasmore.screeninfo.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MessageConfig implements WebMvcConfigurer {
 
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
 
 }
