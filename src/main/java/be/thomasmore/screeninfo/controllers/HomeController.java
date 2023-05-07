56bbc0eb0a2bc42d3b88f101716318f175b876fb
DennyVerschueren <r0952915@student.thomasmore.be>
2023-05-07 23:03:37 +0200

    Email service merge

--- src/main/java/be/thomasmore/screeninfo/controllers/HomeController.java
+++ src/main/java/be/thomasmore/screeninfo/controllers/HomeController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/eventlijst"})
+
+    @GetMapping("/eventlijst")
     public String events(Model model) {
         return "eventlijst";
+    }
+
+    @GetMapping({"/","/start"})
+    public String getInternational() {
+        return "start";
     }
 
 }
