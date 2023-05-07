aa3dcb531f67f1538026713a0cff83f91fb605a1
DennyVerschueren <r0952915@student.thomasmore.be>
2023-05-07 02:38:58 +0200

    Email service made

---
+++ src/main/java/be/thomasmore/screeninfo/model/EmailService.java
@@ -0,0 +1,27 @@
+package be.thomasmore.screeninfo.model;
+
+import be.thomasmore.screeninfo.controllers.LoginController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    private Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+    public void sendEmail(String to, String subject, String body) {
+        logger.info("EMAIL SERVICE TEST AAAAAAAAAAAAAAAAAAAAAAA" );
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+}
