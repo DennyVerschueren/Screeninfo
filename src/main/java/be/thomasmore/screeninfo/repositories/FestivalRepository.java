e0380d6326f63df1e4b6f72299148c63240e9c00
salihtzc <salihtzc.1997@gmail.com>
2023-05-09 13:03:18 +0200

    Festival repository

--- src/main/java/be/thomasmore/screeninfo/repositories/FestivalRepository.java
+++ src/main/java/be/thomasmore/screeninfo/repositories/FestivalRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface FestivalRepository extends CrudRepository<Festival,Integer> {
-
-
+    Optional<Festival> findFirstByOrderByIdAsc();
 
 }
