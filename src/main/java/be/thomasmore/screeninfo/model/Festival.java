8f6a97026324a417ca42d8f943710de200b86acd
salihtzc <salihtzc.1997@gmail.com>
2023-05-09 12:55:29 +0200

    id

--- src/main/java/be/thomasmore/screeninfo/model/Festival.java
+++ src/main/java/be/thomasmore/screeninfo/model/Festival.java
@@ -1,5 +1,14 @@
 package be.thomasmore.screeninfo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Festival {
+
+
+    @Id
+    public Integer id;
+
 
 }
