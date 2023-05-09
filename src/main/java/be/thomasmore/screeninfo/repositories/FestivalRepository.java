0366c9bc636d01ddf61fdd1d2099fc743205c2c2
salihtzc <salihtzc.1997@gmail.com>
2023-05-09 13:01:18 +0200

    Festival repository

---
+++ src/main/java/be/thomasmore/screeninfo/repositories/FestivalRepository.java
@@ -0,0 +1,12 @@
+package be.thomasmore.screeninfo.repositories;
+
+import be.thomasmore.screeninfo.model.Festival;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface FestivalRepository extends CrudRepository<Festival,Integer> {
+
+
+
+}
