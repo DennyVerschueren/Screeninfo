ffa3c7410a3ca6610c7314e024ecbaceabf65712
salihtzc <salihtzc.1997@gmail.com>
2023-05-09 12:58:12 +0200

    Festival class (constructor getter setter)

--- src/main/java/be/thomasmore/screeninfo/model/Festival.java
+++ src/main/java/be/thomasmore/screeninfo/model/Festival.java
@@ -9,6 +9,62 @@ public class Festival {
 
     @Id
     public Integer id;
+    private String festivalName;
+    private String festivalImage;
+    private String festivalDate;
+    private String backgroundColor;
 
+    private String festivalLink;
 
+    public Festival(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFestivalName() {
+        return festivalName;
+    }
+
+    public void setFestivalName(String festivalName) {
+        this.festivalName = festivalName;
+    }
+
+    public String getFestivalImage() {
+        return festivalImage;
+    }
+
+    public void setFestivalImage(String festivalImage) {
+        this.festivalImage = festivalImage;
+    }
+
+    public String getFestivalDate() {
+        return festivalDate;
+    }
+
+    public void setFestivalDate(String festivalDate) {
+        this.festivalDate = festivalDate;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getFestivalLink() {
+        return festivalLink;
+    }
+
+    public void setFestivalLink(String festivalLink) {
+        this.festivalLink = festivalLink;
+    }
 }
