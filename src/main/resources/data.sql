ae79c13cd4bd45236c3020fe051302c3090aa871
salihtzc <salihtzc.1997@gmail.com>
2023-05-09 15:42:59 +0200

    data toegevoegd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,3 +29,15 @@ VALUES (
            4,'VOEDSELKRAAM',35,180,'HotDog Jho','een klein kraampje','KappersLaan');
 
 
+
+
+
+
+INSERT INTO "PUBLIC"."FESTIVAL" VALUES
+                                    (1, '#f28629', '23 - 24.06.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/KOER_484x600.png?itok=_jrPk8Ly', 'https://www.dezomerisvanmechelen.be/evenement/de-koer/info','De Koer'),
+                                    (2, '#6FB746', '29.06 - 17.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/parkpop_2017.jpg?itok=WC0BUTak', 'https://www.dezomerisvanmechelen.be/evenement/parkpop/info','Parkpop'),
+                                    (3, '#253679', '25.08 - 27.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/190823-201655-Maanrock2019-JD%20%28Groot%29.jpg?itok=QF0Db-NJ', 'https://www.maanrock.be/','Maanrock'),
+                                    (4, 'rgb(197, 163, 197)', 'elke eerste zondag van de maand', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/190823-201655-Maanrock2019-JD%20%28Groot%29.jpg?itok=QF0Db-NJ', 'https://shoppenin.mechelen.be/koopzondagen','Koopzondagen');
+
+
+
