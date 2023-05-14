85f37880c9e369267aa1cc2c588c0d7aa8f13154
Lande <r0781198@student.thomasmore.be>
2023-05-14 11:24:09 +0200

    implementing the new data to festival

--- src/main/java/be/thomasmore/screeninfo/controllers/FestivalController.java
+++ src/main/java/be/thomasmore/screeninfo/controllers/FestivalController.java
@@ -1,13 +1,16 @@
 package be.thomasmore.screeninfo.controllers;
 
 import be.thomasmore.screeninfo.model.Festival;
+import be.thomasmore.screeninfo.model.taal.FestivalNederlands;
 import be.thomasmore.screeninfo.repositories.FestivalRepository;
+import be.thomasmore.screeninfo.repositories.taal.FestivalNederlandsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 public class FestivalController {
@@ -15,14 +18,34 @@ public class FestivalController {
     @Autowired
     FestivalRepository festivalRepository;
 
+    @Autowired
+    FestivalNederlandsRepository nederlandsRepository;
+
     @GetMapping("/festivallijst")
     public String festivalList(Model model, Principal principal) {
 
-        if(principal != null){
+        String taal = null;
 
+        if(principal != null){
+            System.out.println("how are you logged in ?");
+        }
+        else{
+            taal = "NEDERLANDS";
         }
 
         Iterable<Festival> festivals = festivalRepository.findAll();
+
+        for (Festival festival: festivals) {
+            if(taal.equals("NEDERLANDS")){
+                Optional<FestivalNederlands> taalDataOptional = nederlandsRepository.findByFestivalId(festival.getId());
+
+                if(taalDataOptional.isPresent()){
+                    FestivalNederlands taalData = taalDataOptional.get();
+                    festival.setFestivalName(taalData.getFestivalName());
+                    festival.setFestivalDate(taalData.getFestivalDate());
+                }
+            }
+        }
 
         model.addAttribute("festivals",festivals);
 
--- src/main/java/be/thomasmore/screeninfo/repositories/taal/FestivalEnglishRepository.java
+++ src/main/java/be/thomasmore/screeninfo/repositories/taal/FestivalEnglishRepository.java
@@ -2,11 +2,12 @@ package be.thomasmore.screeninfo.repositories.taal;
 
 
 import be.thomasmore.screeninfo.model.taal.FestivalEnglish;
+import be.thomasmore.screeninfo.model.taal.FestivalNederlands;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface FestivalEnglishRepository extends CrudRepository<FestivalEnglish,Integer> {
+public interface FestivalEnglishRepository extends CrudRepository<FestivalNederlands,Integer> {
 
     //Optional<FestivalEnglish> SearchByFestivalId(Integer festivalId);
 
