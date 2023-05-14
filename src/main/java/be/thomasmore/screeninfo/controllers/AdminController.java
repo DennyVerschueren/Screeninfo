cd8c80e1b0c43e99a7f07a6922801ffbad0dbcb8
DennyVerschueren <r0952915@student.thomasmore.be>
2023-05-15 01:13:15 +0200

    Events aanmaken funtionaliteit

--- src/main/java/be/thomasmore/screeninfo/controllers/AdminController.java
+++ src/main/java/be/thomasmore/screeninfo/controllers/AdminController.java
@@ -6,11 +6,12 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class AdminController {
@@ -18,26 +19,35 @@ public class AdminController {
     @Autowired
     private FestivalRepository festivalRepository;
 
-    @GetMapping("/admin/festivaleditor")
+    @ModelAttribute("festival")
+    public Festival findParty(@PathVariable(required = false) Integer id) {
+        if (id!=null) {
+            Optional<Festival> optionalFestival = festivalRepository.findById(id);
+            if (optionalFestival.isPresent()) return optionalFestival.get();
+        }
+        return new Festival();
+    }
+
+    @GetMapping({"/festivaleditor"})
     public String editFestival(Model model) {
         model.addAttribute("festivals", festivalRepository.findAll());
         return "admin/festivaleditor";
     }
 
-    @PostMapping("/admin/festivaleditor")
-    public String editFestivalPost(Model model, @Valid Festival festival) {
+    @PostMapping("/festivaleditor")
+    public String editFestivalPost(@Valid Festival festival) {
         festivalRepository.save(festival);
-        return "redirect:/festivallijst/";
+        return "redirect:festivallijst/";
     }
 
-    @GetMapping("/admin/festivalcreator")
+    @GetMapping("/festivalcreator")
     public String addFestival() {
         return "admin/festivalcreator";
     }
 
-    @PostMapping("/admin/festivalcreator")
+    @PostMapping("/festivalcreator")
     public String addFestivalPost(Model model, @Valid Festival festival) {
         festivalRepository.save(festival);
-        return "redirect:/festivallijst/";
+        return "redirect:festivallijst/";
     }
 }
