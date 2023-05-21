feb72459a4c0bd937168b3baa137e427b057ed8b
Lande <r0781198@student.thomasmore.be>
2023-05-21 09:43:39 +0200

    elke soort van de barometer ingestoken

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,7 +33,7 @@ VALUES (
 
 
 INSERT INTO "PUBLIC"."FESTIVAL" VALUES
-                                    (1, '#f28629', '23 - 24.06.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/KOER_484x600.png?itok=_jrPk8Ly', 'https://www.dezomerisvanmechelen.be/evenement/de-koer/info','De Koer',10000,0, false),
-                                    (2, '#6FB746', '29.06 - 17.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/parkpop_2017.jpg?itok=WC0BUTak', 'https://www.dezomerisvanmechelen.be/evenement/parkpop/info','Parkpop',1000,0, false),
-                                    (3, '#253679', '25.08 - 27.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/190823-201655-Maanrock2019-JD%20%28Groot%29.jpg?itok=QF0Db-NJ', 'https://www.maanrock.be/','Maanrock',1000,0, false),
-                                    (4, '#4e8496', '25.08 - 27.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/190823-201655-Maanrock2019-JD%20%28Groot%29.jpg?itok=QF0Db-NJ', null,'Momenteele Event',1000,750 , true);
+                                    (1, '#f28629', '23 - 24.06.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/KOER_484x600.png?itok=_jrPk8Ly', 'https://www.dezomerisvanmechelen.be/evenement/de-koer/info','De Koer',10000,false,0),
+                                    (2, '#6FB746', '29.06 - 17.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/parkpop_2017.jpg?itok=WC0BUTak', 'https://www.dezomerisvanmechelen.be/evenement/parkpop/info','Parkpop',1000,false,0),
+                                    (3, '#253679', '25.08 - 27.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/190823-201655-Maanrock2019-JD%20%28Groot%29.jpg?itok=QF0Db-NJ', 'https://www.maanrock.be/','Maanrock',1000,false,0),
+                                    (4, '#4e8496', '25.08 - 27.08.2023', 'https://www.dezomerisvanmechelen.be/sites/default/files/styles/event_cover_front/public/events/cover/190823-201655-Maanrock2019-JD%20%28Groot%29.jpg?itok=QF0Db-NJ', null,'Momenteele Event',1000,true,756);
