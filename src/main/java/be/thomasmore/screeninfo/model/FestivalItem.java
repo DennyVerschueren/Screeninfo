556eed95205647e4b4ee921ff5e83e547c2db669
Lande <r0781198@student.thomasmore.be>
2023-05-23 10:06:32 +0200

    een nieuwe classe (FestivalItem) aangemaakt voor html

---
+++ src/main/java/be/thomasmore/screeninfo/model/FestivalItem.java
@@ -0,0 +1,63 @@
+package be.thomasmore.screeninfo.model;
+
+import java.util.Date;
+
+public class FestivalItem {
+    private String festivalName;
+    private String festivalImage;
+    private String backgroundColor;
+    private Date festivalDate;
+    private String festivalLink;
+    private boolean onGoing; // om manueel te zeggen dat een event bezig is
+
+    public FestivalItem(Festival festival){
+    }
+
+    public String getFestivalName() {
+        return festivalName;
+    }
+
+    public void setFestivalName(String festivalName) {
+        this.festivalName = festivalName;
+    }
+
+    public String getFestivalImage() {
+        return festivalImage;
+    }
+
+    public void setFestivalImage(String festivalImage) {
+        this.festivalImage = festivalImage;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public void setFestivalDate(Date festivalDate) {
+        this.festivalDate = festivalDate;
+    }
+
+    public Date getFestivalDate() {
+        return festivalDate;
+    }
+
+    public void setFestivalLink(String festivalLink) {
+        this.festivalLink = festivalLink;
+    }
+
+    public String getFestivalLink() {
+        return festivalLink;
+    }
+
+    public void setOnGoing(boolean onGoing) {
+        this.onGoing = onGoing;
+    }
+
+    public boolean isOnGoing() {
+        return onGoing;
+    }
+}
