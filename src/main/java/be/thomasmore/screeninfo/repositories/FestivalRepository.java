b12865084481be21b7631203b840fafd68d9d90b
salihtzc <salihtzc.1997@gmail.com>
2023-05-24 12:33:25 +0200

    filter query

--- src/main/java/be/thomasmore/screeninfo/repositories/FestivalRepository.java
+++ src/main/java/be/thomasmore/screeninfo/repositories/FestivalRepository.java
@@ -1,8 +1,11 @@
 package be.thomasmore.screeninfo.repositories;
 
 import be.thomasmore.screeninfo.model.Festival;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FestivalRepository extends CrudRepository<Festival,Integer> {
@@ -11,4 +14,8 @@ public interface FestivalRepository extends CrudRepository<Festival,Integer> {
     Optional<Festival> findFirstByIdLessThanOrderByIdDesc(int id);
     Optional<Festival> findFirstByIdGreaterThanOrderById(int id);
     Iterable<Festival> findAllByOrderByOnGoingDesc();
+
+
+    @Query("SELECT a FROM Festival a WHERE :word IS NULL OR LOWER(a.festivalName) LIKE LOWER(CONCAT('%',:word,'%'))")
+    List<Festival> findByKeyword(@Param("word") String word);
 }
