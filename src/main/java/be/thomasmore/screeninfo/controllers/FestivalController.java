f7963af77cbb289573a2690db3438d4277fc0292
salihtzc <salihtzc.1997@gmail.com>
2023-05-24 12:35:08 +0200

    logger + getmapping voor filter

--- src/main/java/be/thomasmore/screeninfo/controllers/FestivalController.java
+++ src/main/java/be/thomasmore/screeninfo/controllers/FestivalController.java
@@ -1,19 +1,24 @@
 package be.thomasmore.screeninfo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import be.thomasmore.screeninfo.model.Festival;
 import be.thomasmore.screeninfo.repositories.FestivalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class FestivalController {
 
+    private Logger logger = LoggerFactory.getLogger(FestivalController.class);
     @Autowired
-    FestivalRepository festivalRepository;
+    private FestivalRepository festivalRepository;
 
     @GetMapping({"/","/festivallijst"})
     public String festivalList(Model model, Principal principal) {
@@ -24,8 +29,12 @@ public class FestivalController {
         return "festivallijst";
     }
 
-    @GetMapping("/festivallijst/{filter}")
-    public String festivalListWithFilter(Model model){
+    @GetMapping("/festivallijst/filter")
+    public String festivalListWithFilter(Model model,@RequestParam(required = false) String keyword){
+        logger.info("festivalListWithFilter -- keyword=" + keyword);
+        model.addAttribute("keyword", keyword);
+        List<Festival> festivals = festivalRepository.findByFestivalNameContainingIgnoreCase(keyword);
+        model.addAttribute("festivals",festivals);
         return "festivallijst";
     }
 
